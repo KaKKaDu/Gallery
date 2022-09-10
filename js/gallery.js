3c6ffd51ce19fdcfb41e307eb6edea8af078bb98
KaKKaDu <kirrakorrt@gmail.com>
2022-09-10 15:21:35 +0300

    perfected section change

--- js/gallery.js
+++ js/gallery.js
@@ -2,17 +2,17 @@
 
 let navig = $("#navig");
 let buton = $("#buton");
-let sect = $("#sect");
+window.sectop = document.getElementById("sect");
 let plus = $("#plus");
 let navigation = document.querySelector('.navbar-nav');
 let morelink = document.getElementsByClassName('morelink');
-let opt = $("#opt");
+let opt = document.getElementById("opt");;
 
 let openclose = function () {
 	navig.toggleClass('navbshow');
 	buton.toggleClass('btnopen');
 	plus.toggleClass('plusopen');
-	sect.toggleClass('sectop');
+	sectop.classList.toggle('sectop');
 }
 
 function newlibrary(html) {
@@ -21,37 +21,84 @@ function newlibrary(html) {
 			return template.content.firstElementChild; 
 		}
 
+
+window.currentSectionId='sect';
+window.currentButId = 'butdef';
+let butdef = document.getElementById('butdef');
+let sectdef = document.getElementById('sect');
+
 function makeid() {
 	return "_" + Math.random().toString(36).substr(2,9);
 }
 
 let makelib = function(){
+	let id = makeid();
 	let lib = newlibrary(`
 			<li class="nav-item row">
-				<a href="javascript:void(0)" class="nav-link col-8 offset-1">New</a>
+				<a href="javascript:void(0)" class="nav-link col-8 offset-1">New ` + id +`</a>
 				<a href="javascript:void(0)" class="col-3 morelink"><i class="fa-solid fa-ellipsis"></i></a>
-				<ul class="option col-12 hidde" id="opt">
-					<li class="optli">
+				<ul class="option col-12 hidde">
+					<li class="optli" id = 'delete-` + id + `'>
 						<a href="javascript:void(0)">Delete</a>
 					</li>
-					<li class="optli">
+					<li class="optli" id = 'rename-` + id + `'>
 						<a href="javascript:void(0)">Rename</a>
 					</li>
 				</ul>
 			</li>
 		`);
-	lib.setAttribute('id', makeid());
+	console.log(lib);
+	
+	lib.setAttribute('id', 'menu-' + id);
 	navigation.appendChild(lib);
-	console.log(morelink);
 	let section = newlibrary(`
 		<section id="sect" class='hidde'>
 			<div class="nothing">
-				<h2>This library is empty. Add your first photo!</h2>
+				<h2>This library is empty. Add your first photo! `+ id +`</h2>
 			</div>
 		</section>
 		`)
-	section.setAttribute('id', makeid());
+	section.setAttribute('id', 'section-' + id);
 	document.body.appendChild(section);
+
+
+	lib.onclick = function(event) {
+		const prevbut = document.getElementById(window.currentButId);
+		prevbut.classList.remove('libraryfoc');
+
+		const but = document.getElementById('menu-' + id);
+		but.classList.add('libraryfoc');
+		window.currentButId = ('menu-' + id);
+
+		const prevsection = document.getElementById(window.currentSectionId); 
+		prevsection.classList.add('hidde');
+		prevsection.classList.remove('currentsect');
+		prevsection.classList.remove('sectop');
+
+		const section = document.getElementById('section-' + id);
+		window.sectop = document.getElementById('section-' + id);
+		section.classList.remove('hidde');
+		section.classList.add('currentsect');
+		section.classList.add('sectop');
+		window.currentSectionId = ('section-' + id); 
+	}
+}
+
+butdef.onclick = function(event) {
+	const prevbut = document.getElementById(window.currentButId);
+	prevbut.classList.remove('libraryfoc');
+	butdef.classList.add('libraryfoc');
+	window.currentButId = ('butdef');
+
+	const prevsection = document.getElementById(window.currentSectionId); 
+	prevsection.classList.add('hidde');
+	prevsection.classList.remove('currentsect');
+	prevsection.classList.remove('sectop');
+	window.sectop = sectdef;
+	sectdef.classList.remove('hidde');
+	sectdef.classList.add('currentsect');
+	sectdef.classList.add('sectop');
+	window.currentSectionId = 'sect';
 }
 
 navigation.addEventListener('click', function(e) {
@@ -62,20 +109,9 @@ navigation.addEventListener('click', function(e) {
 	}
 })
 
-
-/*for (let i = 0; i < morelink.length; i++) {
-  	morelink[i].addEventListener('click', function() {
-  	alert('Спасибо!');
-  })
-
-  if (e.target.classList.contains('morelink')) {
-		console.log(e.target.parentNode);
-	}
-}*/
-
-
-
-/*morelink.addEventListener('click', function(e) {
-	let libid = e.target.parentNode;
-	console.log(libid);
-})*/
+/*<li class="optli" id = 'delete-` + id + `'>
+						<a href="javascript:void(0)">Delete</a>
+					</li>
+					<li class="optli" id = 'rename-` + id + `'>
+						<a href="javascript:void(0)">Rename</a>
+					</li>*/
